4c240ae6c01c82bd2f7b6b87771891900605faa3
MaksymovIvan <vani4kamaksymov.2006@gmil.com>
2025-04-17 21:03:29 +0300

    GUI

--- Expense-Tracker/src/app/Main.java
+++ Expense-Tracker/src/app/Main.java
@@ -1,11 +1,28 @@
 package app;
 
+import com.formdev.flatlaf.FlatDarculaLaf;
+import com.formdev.flatlaf.FlatLaf;
+
 import javax.swing.*;
+import java.awt.*;
 
 public class Main {
     public static void main(String[] args) {
-        SwingUtilities.invokeLater(() -> {
-            new MainWindow().setVisible(true);
-        });
+        try {
+            FlatLaf.registerCustomDefaultsSource("resources");
+
+            UIManager.setLookAndFeel(new FlatDarculaLaf());
+
+            UIManager.put("defaultFont", new Font("Segoe UI", Font.PLAIN, 14));
+
+            UIManager.put("Component.arc", 12);
+            UIManager.put("Button.arc", 12);
+            UIManager.put("Component.focusWidth", 2);
+
+        } catch (Exception e) {
+            System.err.println("Не вдалося застосувати FlatLaf");
+        }
+
+        SwingUtilities.invokeLater(() -> new MainWindow().setVisible(true));
     }
 }
